8460aaf6211fa338c004e10fe08d91024bab4371
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-05-20 22:53:09 +0430

    creat new task function added to tasks.service.ts

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
-
+import { Task, TaskStatus } from './task.model';
+import * as uuid from 'uuid';
 @Injectable()
 export class TasksService {
     private tasks:Task[] = [];
 
     getAllTasks():Task[]{
         return this.tasks;
+    }
+    creatTask(title:string , description:string): Task{
+        const task: Task={
+            id:uuid(),
+            title,
+            description,
+            status:TaskStatus.TODO
+        }
+        this.tasks.push(task);
+        return task;
     }
 }
