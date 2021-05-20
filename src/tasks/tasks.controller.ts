7d3005bf853545267d9c12358242ff35a7d1b77b
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-05-20 23:02:17 +0430

    creat new task function done

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -11,5 +11,12 @@ export class TasksController {
         return this.taskService.getAllTasks();
     }
 
+    @Post()
+    creatTask(
+       @Body('title') title:string,
+       @Body('description') description:string,
+    ): Task{
+     return   this.taskService.creatTask(title,description);
+    }
 
 }
