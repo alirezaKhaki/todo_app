6a4212124ecc954d627d4f36554368fa665935be
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-05-21 17:39:19 +0430

    signup function created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthDto } from './dto/auth.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository:UserRepository,
-    ){}
+        private userRepository: UserRepository,
+    ) { }
+
+    async signUp(authDto: AuthDto): Promise<any> {
+        return this.userRepository.signUp(authDto);
+    }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,18 @@
 import { EntityRepository, Repository } from "typeorm";
+import { AuthDto } from "./dto/auth.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
+    async signUp(authDto: AuthDto): Promise<any> {
+        const { username, password } = authDto;
 
+        const user = new User();
+        user.username = username;
+        user.password = password;
+        
+        await user.save();
+
+        return { "msg": "user created" }
+    }
 }
