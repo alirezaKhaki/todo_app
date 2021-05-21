c5c68ccbbeb41cc2cf724d0732728d0279c6de53
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-05-21 21:09:27 +0430

    jwt strategy added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,9 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 @Module({
   imports: [PassportModule.register({
     defaultStrategy: 'jwt'
@@ -16,7 +17,11 @@ import { PassportModule } from '@nestjs/passport';
     }
   })
     , TypeOrmModule.forFeature([UserRepository])],
-  providers: [AuthService],
-  controllers: [AuthController]
+  providers: [AuthService,JwtStrategy],
+  controllers: [AuthController],
+  exports:[
+    JwtStrategy,
+    PassportModule,
+  ]
 })
 export class AuthModule { }
