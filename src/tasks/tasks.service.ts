1a2188d47d35cfd67a034d40adc0b7ffc7079e9c
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-05-21 12:46:13 +0430

    error handeling not found for delete task function

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -35,6 +35,7 @@ export class TasksService {
         return task;
     }
     deleteTaskById(id: string): void {
-        this.tasks = this.tasks.filter(task => task.id !== id)
+        const found=this.getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id !== found.id)
     }
 }
