08c3636fc9b1ffa7d39dceefb06d19b12c32759a
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-05-21 19:16:29 +0430

    jwt module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 
 @Module({
-    imports:[
+    imports: [
+        PassportModule.register({
+            defaultStrategy:'jwt'
+        }),
+        JwtModule.register({
+            secret: '123456789',
+            signOptions: {
+                expiresIn: 3600,
+            }
+        }),
         TypeOrmModule.forRoot(typeOrmConfig),
         TasksModule,
         AuthModule],
 })
 
-export class AppModule {}
+export class AppModule { }
