5d30f67216fb547a57d88fa5037cc0e036cb465b
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-05-22 22:24:11 +0430

    dockerfile and docker compose created

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: 'postgres://postgres:postgres@localhost/tasks',
+      url: 'postgres://username:password@localhost/tasks',
       autoLoadEntities: true,
       synchronize: true
     }),
