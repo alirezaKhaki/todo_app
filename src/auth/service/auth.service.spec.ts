3107bce17f3b11beffe703f0d6a421807d307235
Alireza Mansourkhaki <a.mansourkhaki@avakatan.ir>
2022-09-05 15:04:00 +0430

    improve test

--- src/auth/service/auth.service.spec.ts
+++ src/auth/service/auth.service.spec.ts
@@ -1,6 +1,7 @@
 import { UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import exp from 'constants';
 import { AuthCredentialsDto } from '../dto/auth.dto';
 import { UserRepository } from '../repository/user.repository';
 import { AuthService } from './auth.service';
@@ -8,12 +9,13 @@ import { AuthService } from './auth.service';
 describe('AuthService', () => {
     let authService: AuthService;
     const authCredentialsDto: AuthCredentialsDto = { username: "alireza", password: "password" };
+
     const mockUserRepository = {
         signUp: jest.fn((data: AuthCredentialsDto) => Promise.resolve(data)),
         validateUserPassword: jest.fn((data: AuthCredentialsDto) => {
             if (data.username == 'alireza')
                 return Promise.resolve(data.username);
-            Promise.reject('Invalid credentials');
+            throw new UnauthorizedException('Invalid credentials');
         })
     };
 
@@ -22,34 +24,71 @@ describe('AuthService', () => {
     };
 
     beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [AuthService, UserRepository, JwtService],
-        })
-            .overrideProvider(UserRepository).useValue(mockUserRepository)
-            .overrideProvider(JwtService).useValue(mockJwtService)
-            .compile();
+        try {
+            const module: TestingModule = await Test.createTestingModule({
+                providers: [AuthService, UserRepository, JwtService],
+            })
+                .overrideProvider(UserRepository).useValue(mockUserRepository)
+                .overrideProvider(JwtService).useValue(mockJwtService)
+                .compile();
 
-        authService = module.get<AuthService>(AuthService);
+            authService = module.get<AuthService>(AuthService);
+        } catch (error) {
+
+        };
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        jest.restoreAllMocks();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+        jest.restoreAllMocks();
     });
 
     it('should be defined', () => {
         expect(authService).toBeDefined();
     });
 
+    it('repository should be defined', () => {
+        expect(mockUserRepository).toBeDefined();
+    });
+
     it('should signUp user', async () => {
-        const data = await (authService.signUp(authCredentialsDto));
-        expect(data).toEqual(authCredentialsDto);
+        try {
+            const data = await (authService.signUp(authCredentialsDto));
+
+            expect(data).toEqual(authCredentialsDto);
+
+            expect(mockUserRepository.signUp).toHaveBeenCalled();
+            expect(mockUserRepository.signUp).toHaveBeenCalledWith(authCredentialsDto);
+        } catch (error) {
+            console.error(error);
+        };
     });
 
     it('should sign in', async () => {
-        const data = await (authService.signIn(authCredentialsDto));
-        expect(data).toEqual({ accessToken: "access" });
+        try {
+            const data = await (authService.signIn(authCredentialsDto));
+
+            expect(data).toEqual({ accessToken: "access" });
+
+            expect(mockUserRepository.validateUserPassword).toHaveBeenCalled();
+            expect(mockUserRepository.validateUserPassword).toHaveBeenCalledWith(authCredentialsDto);
+        } catch (error) {
+            console.log(error);
+        }
     });
 
     it('should throw Invalid credentials errors on signIn', async () => {
         try {
             const diffrentUser: AuthCredentialsDto = { username: 'diffrent user', password: "password" }
             const data = await (authService.signIn(diffrentUser));
+
+            expect(mockUserRepository.validateUserPassword).toHaveBeenCalled();
+            expect(mockUserRepository.validateUserPassword).toHaveBeenCalledWith(diffrentUser);
         } catch (error) {
             expect(error).toBeInstanceOf(UnauthorizedException);
         };
